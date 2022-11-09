078e6c85987618727aa88fa6b8f9fe9632a91b69
Tomasz1235l <108531577+Tomasz1235l@users.noreply.github.com>
2022-11-09 09:03:09 +0100

    Update wsl.bat

--- versions/2.0/wsl.bat
+++ versions/2.0/wsl.bat
@@ -1,28 +1,64 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdDWDJFGL+UcMPBRZTReYAFu1BKcd5u3H5umIrAMUV+1f
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJiZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsGAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQIGBVtgWRaAMws=
+::eg0/rx1wNQPfEVWB+kM9LVsJDBORMCW4B6F8
+::fBEirQZwNQPfEVWB+kM9LVsJDBORMCusC/sZ8O2b
+::cRolqwZ3JBvQF1fEqQI/Ph5GQAWbfG21FPUL++Sb
+::dhA7uBVwLU+EWFKN7ksxaBJaT0ri
+::YQ03rBFzNR3SWATE+k0gLXs=
+::dhAmsQZ3MwfNWATE+k0gLXs=
+::ZQ0/vhVqMQ3MEVWAtB9wJhRESWQ=
+::Zg8zqx1/OA3MEVWAtB9wJhRESWQ=
+::dhA7pRFwIByZRRmL4kciJBpNDAKNLiutFblc/+Hv56qCpEsYV/Z/bICb17WXYPIW60mkdpkgxm9Ondls
+::Zh4grVQjdCyDJEyX8VAjFA9bQQWRJleKE7kM4fzH+PmL4lsQBqw6YIq7
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 color 07
-title WSL VM Turbo - running!
+titile wsl vm - running
 
 :start
 cls
-echo  /\  CPU: OK   OVERCLOCK: 200MH
-echo /  \ RAM: OK   4GB
-echo \  / GPU: OK   STAT: NORMAL
-echo  \/  NET: OK   INET: 192.168.0.%random%
-timeout /t 3
+echo   /\    CPU: OK    OVERCLOCK: 200MH
+echo  /  \   RAM: OK    2GB
+echo  \  /   GPU: OK    STAT: NORMAL
+echo   \/    NET: OK    INET: 192.168.0.%random%
+timeout /t 3 >nul
 goto grub
 
-:WIN
+:grub
 cls
-echo reading ramdisk...
+echo [=========]
+echo [WSL      ]1
+echo [WSL recov]2
+echo [DOS      ]3
+echo [WIN      ]4
+echo [=========]
+set /p"sys=grub>"
+cls
+if /I %sys% equ 1 goto WSL
+if /I %sys% equ 2 goto REC
+if /I %sys% equ 3 goto DOS
+if /I %sys% equ 4 goto WIN
 timeout /t 3 >nul
-exit
-
-:MS-DOS
-cls
-echo reading ramdisk...
-timeout /t 3 >nul
-cls
-call cmd.exe
 
 :WSL
 cls
@@ -31,24 +67,23 @@ timeout /t 3 >nul
 cls
 wsl
 
-:1
+:REC
 cls
 echo reading ramdisk...
 timeout /t 3 >nul
 cls
-echo WSL RECOVERY:
 wsl bash -norc -noprofile --login
 
-:GRUB
+:DOS
 cls
-echo [======]
-echo [WSL   ]
-echo [WSL RE]
-echo [DOS   ]
-echo [WIN   ]
-echo [======]
-set /p"sys=grub>"
-if /I %sys% equ 1 goto WSL
-if /I %sys% equ 2 goto 1
-if /I %sys% equ 3 goto DOS
-if /I %sys% equ 4 goto WIN
+echo reading ramdisk...
+timeout /t 3 >nul
+cls
+call cmd.exe
+
+:WIN
+cls
+echo reading ramdisk...
+timeout /t 3 >nul
+cls
+exit
